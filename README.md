3714ce8cec6b7b3d79ca4cabb343a14659855beb
Kendall <kmolas@nyit.edu>
2017-11-25 02:45:03 -0500

    Foundation done

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-## This don't work yet
+# Round Robin CPU Scheduler
 
-It is still a work in progress.
+## Introduction
+The RR (Round-Robin CPU Scheduler) is a scheduler in which the CPU will run a certain a process for
+some specific time quantum which is determined by the user. Upon arrival of the process it will be
+run for that specified time quantum. This process is repeated with any new processes that are put
+into the queue.
+
+## Assignment
+Create a CPU Scheduler from scratch that will read a csv and implement this algorithm.
+
+## Future:
+- Calculate waiting time, turnaround time (context switching)
+- Come up with final assessment of efficiency of the scheduler
+- Possible gui
+
+It is still a work in progress
+
+
--- Round Robin.c
+++ Round Robin.c
@@ -4,14 +4,9 @@
 #include <time.h>
 
 /* To do:
- * - Shift main() to top and use prototyping effectively
- * 
  * - Dequeueing from queue 
- * 		based on queue, process the ids for running the time quantum
- * 		calculate waiting time, turnaround time (context switching)
- * 
- * - Sort based on arrival time
- * - After completing burst time, remove from queue
+ * 	 based on queue, process the ids for running the time quantum
+ * 	 calculate waiting time, turnaround time (context switching)
  * 
  */
  
@@ -23,8 +18,9 @@ void delay(int ms);
 void read_csv();
 void enqueue(Queue *q, Process *p);
 void dequeue(Queue *q);
-int check_current_size(Queue *q);
+void getCurrentState(Queue *q);
 void start_scheduler(Queue *q, unsigned *tq);
+int check_current_size(Queue *q);
 int compute_burst_time(Process *p, unsigned *tq);
 Process *process_pids(unsigned *pid, unsigned *arrival, unsigned *burst);
 
@@ -46,7 +42,7 @@ typedef struct queue {
     Process *arr[4];	// Pointer to the array as we don't know what array we're specifying
     int front;
     int rear;
-    int size;
+    size_t size;
 } Queue;
 
 //--------------------------------------------------------------------------------------------------
@@ -54,7 +50,8 @@ typedef struct queue {
  * - Check current size of queue
  * - Enqueue
  * - Dequeue
- * - Get current state of queue */
+ * - Get current state of queue 
+ * - Sort queue in ascending order based off of arrival time*/
 //--------------------------------------------------------------------------------------------------
 
 // This function returns amount of items currently in the ready queue
@@ -73,51 +70,61 @@ void enqueue(Queue *queue, Process *process) {
 	   queue->arr[currentSize] = process;
 	   queue->size++;
 	}
-
-   //printf("Pid: %d, Arr: %d Burs %d\n", process->pid, process->arrival_time, process->burst_time);
 }
 
-// This functions either: 
+// This function either: 
 // Shift processes in array by 1 and move front process to the end if process is not null OR
 // Dequeues process if process' burst time is zero
+// Problems: Does not make it null
 void dequeue(Queue *queue) {
 	queue->front = 0;
-	queue->rear = 3;
+	queue->rear = queue->size-1;
 	Process *tempProcess = queue->arr[queue->front]; 
 	
 	// Check if the process in front of the queue is not considered NULL
 	// Move front process to the end
 	if (tempProcess != NULL) {
-		for (size_t i = 1; i < (size_t) queue->size; i++) {
-			queue->arr[i-1] = queue->arr[i];
+		for (size_t i = 0; i < queue->size-1; i++) {
+			queue->arr[i] = queue->arr[i+1];
+			printf("--\n");
 		}
 		queue->arr[queue->rear] = tempProcess;
 	}
 	// Since it's null, dequeue it permanently
 	else {
-		for (size_t i = 0; i < (size_t) queue->size-1; i++) {
+		for (size_t i = 0; i < queue->size-1; i++) {
 			queue->arr[i] = queue->arr[i+1]; 
 		}
 		queue->size--;
-		printf("Process has been removed from queue");
+		printf("Process has been removed from queue\n");
 	}
 }
 
-// After the program runs for specified
-// time quantum, check the current state of all processes
-void getCurrentState(Queue *queue) {
-	for (size_t i = 0; i < (size_t) queue->size; i++) {
-		printf("Process id#: %d | Arrival Time: %d | Burst Time %d\n", queue->arr[i]->pid, 
-		queue->arr[i]->arrival_time, queue->arr[i]->burst_time);
-		delay(1000);
+// After the program runs for specified time quantum, check the current state of all processes
+void getCurrentState(Queue *q) {
+	for (size_t i = 0; i < q->size; i++) {
+		printf("Process id#: %d | Arrival Time: %d | Burst Time %d\n", q->arr[i]->pid, 
+		q->arr[i]->arrival_time, q->arr[i]->burst_time);
+	}
+}
+
+void sortQueue(Queue *q) {
+	for (size_t i = 0; i < q->size; i++) {
+		for (size_t j = 0; j < q->size-1; j++) {
+			if (q->arr[j+1]->arrival_time < q->arr[j]->arrival_time) {
+				Process *p = q->arr[j];
+				q->arr[j] = q->arr[j+1];
+				q->arr[j+1] = p;
+			}
+		}
 	}
 }
 
 //--------------------------------------------------------------------------------------------------
 // Methods needed for starting Round Robin CPU Scheduling
 // Process the processes in the ready queue with specified time quantum, etc
+// @returns pointer to process with specified id, arrival time, and burst time
 //--------------------------------------------------------------------------------------------------
-
 Process *process_pids(unsigned *p_id, unsigned *arrival, unsigned *burst) {
     Process *p;
     p = malloc(sizeof(Process));
@@ -140,28 +147,29 @@ void read_csv(Queue *ready_queue) {
 
     file = fopen("pid.csv","r"); // Read the csv file
 
+	// Check if file exists and if it does, continue reading ntil end of file
 	if (file) {
 		while ((c = getc(file)) != EOF) {
-			if (isdigit(c)) {
+			if (isdigit(c)) {	// Check if character read is a number
 				if (c != ',') {
+					
+					// Read each number and store it after recognizing the ','
 					if (pidFound == 0 && (arrivalTimeFound == burstTimeFound)) {
-						process_id = c - '0';
-						//printf("Process id: %d", process_id);			// Debugging
+						process_id = c - '0';	// Needed to turn read character in a number
 						pidFound = 1;
 					}
 					else if (arrivalTimeFound == 0 && pidFound == 1) {
 						arrival_time = c - '0';
-						//printf(" Arrival: %d", arrival_time);		// Debugging
 						arrivalTimeFound = 1;
 					}
 					else if (burstTimeFound == 0 && (arrivalTimeFound == pidFound)) {
 						burst_time = c - '0';
-						//printf(" Burst Time: %d", burst_time);			// Debugging
 						burstTimeFound = 1;
-						//printf("\n");
 					}
 				}
 			}
+			
+			// Begin enqueueing the processes
 			else if (c == '\n' && (pidFound == 1 && burstTimeFound == 1 && arrivalTimeFound == 1 )) {
 				enqueue(ready_queue, process_pids(&process_id, &arrival_time, &burst_time));
 				pidFound = 0, arrivalTimeFound = 0, burstTimeFound = 0;
@@ -177,25 +185,26 @@ void start_scheduler(Queue *ready_queue, unsigned *time_quantum) {
 	Process *newProcess = ready_queue->arr[ready_queue->front];
 	while (newProcess != NULL) {
 		if (newProcess->burst_time > 0) {
+			printf("Processing...\n");
 			newProcess->burst_time = compute_burst_time(newProcess, time_quantum);
-			if (newProcess->burst_time == 0) {
-				newProcess = NULL;
-			}
-			printf("Process ran for time quantum of: %d\n", *time_quantum);
-			printf("Current size of queue is: %d\n",check_current_size(ready_queue));
+			printf("\n");
+			
 			getCurrentState(ready_queue);
+			printf("\n");
+			if (newProcess->burst_time == 0) {
+				printf("Removing from queue\n\n");
+				ready_queue->arr[ready_queue->front] = NULL;
+			}
 			dequeue(ready_queue);
+			printf("Process ran for time quantum of: %d\nCurrent size of the queue is:%d\n"
+			"After dequeue, state of processes looks as follows.\n", *time_quantum, check_current_size(ready_queue));
+			getCurrentState(ready_queue);
 			
 			// Set the process equal to whatever is in front now
 			newProcess = ready_queue->arr[ready_queue->front];
 			printf("\n");
 			delay(1000);
 		}
-		/*
-		else {
-			newProcess = NULL;
-			ready_queue->size--;
-		}*/
 	}
 }
 
@@ -206,17 +215,17 @@ int compute_burst_time(Process *p, unsigned *time_quantum) {
 	for (size_t i = *time_quantum; i > 0; i--) {
 		if (p->burst_time != 0) {
 			p->burst_time -= 1;
-			printf("Burst time decremented\n");
+			printf("Running on CPU... Burst Time Decremented...\n");
 			delay(1000);
 		}
 	}
 	return p->burst_time;
-	
 }
 
-// Other crap
-void delay(int milliseconds)
-{
+//--------------------------------------------------------------------------------------------------
+// Debugging tools
+//--------------------------------------------------------------------------------------------------
+void delay(int milliseconds) {
     long pause;
     clock_t now,then;
 
@@ -226,29 +235,25 @@ void delay(int milliseconds)
         now = clock();
 }
 
-// Start
 int main() {
 	
 	// Originally initialize Array to contain null values
-    Queue qtest = {NULL};
+    Queue ready_queue = {NULL};
+    read_csv(&ready_queue);
+	printf("Size is: %d\n", check_current_size(&ready_queue));
+    sortQueue(&ready_queue);
     
-    read_csv(&qtest);
     printf("Enter the time quantum of your choice ");
     scanf("%d", &time_quantum);
     printf("Your time quantum is: %d\n", time_quantum);
-
-    printf("Size is: %d\n", check_current_size(&qtest));
-    printf("Processes loaded into ready queue:\n");
     
-    // It works
-    for (size_t i = 0; i < check_current_size(&qtest); i++) {
-		printf("Process id#: %d | Arrival Time: %d | Burst Time %d\n", qtest.arr[i]-> pid, 
-		qtest.arr[i] -> arrival_time, qtest.arr[i]->burst_time);
-    }
+    printf("Size is: %d\nProcesses loaded into ready queue:\n", check_current_size(&ready_queue));
+    
+    getCurrentState(&ready_queue);
     
     printf("\n");
-    
-	start_scheduler(&qtest, &time_quantum);
+	start_scheduler(&ready_queue, &time_quantum);
+	printf("Done processing\n");
 	exit(0);
 }
 
