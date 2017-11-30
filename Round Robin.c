7dc08e96d8f78d9f9c91e4c1ac9e96f61eb38030
Kendall <kmolas@nyit.edu>
2017-11-30 02:54:16 -0500

    Changes

--- Round Robin.c
+++ Round Robin.c
@@ -1,27 +1,21 @@
+// Kendall Molas
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
 
-/* To do:
- * - Dequeueing from queue 
- * 	 based on queue, process the ids for running the time quantum
- * 	 calculate waiting time, turnaround time (context switching)
- * - CPU Utilization
- */
- 
 typedef struct processes Process;
 typedef struct queue Queue;
 typedef struct cpu CPU;
 unsigned time_quantum;
 
 void delay(int ms);
-void read_csv();
+void read_csv(Queue *q, char *file);
 void enqueue(Queue *q, Process *p);
 void dequeue(Queue *q, CPU *processor, time_t *exec_time);
 void getCurrentState(Queue *q);
 void start_scheduler(CPU *cpu, Queue *q, unsigned *tq);
-void calculate_process_info(Process *p, double *exec_time);
+void calculate_process_info(Process *p, int *exec_time);
 
 double cpu_maximum_utilization(CPU *cpu);
 int check_current_size(Queue *q);
@@ -48,7 +42,7 @@ typedef struct queue {
     Process *arr[4];	// Pointer to the array as we don't know what array we're specifying
     int front;
     int rear;
-    size_t original_size, size;
+    size_t original_size, current_size;
 } Queue;
 
 typedef struct cpu{
@@ -63,27 +57,22 @@ typedef struct cpu{
 //--------------------------------------------------------------------------------------------------
 
 // Prints out the completion time, turnaround time, and waiting time for the process.
-void calculate_process_info(Process *p, double *exec_time) {
+void calculate_process_info(Process *p, int *exec_time) {
 	
-	// Calculate Turnaround time
+	// Calculate Turnaround time & Waiting time
 	p->turnaround_time = *exec_time - p->arrival_time;
-	printf("Execution time: %f and Arrival Time: %d\n", *exec_time, p->arrival_time);
-	
-	// Calculate Waiting time
+	//printf("Exec time: %d and Arrival Time %d\n", *exec_time, p->arrival_time);
 	p->waiting_time = p->turnaround_time - p->completion_time;
-	
-	printf("Process Completion Time: %ds\n", p->completion_time);
-	printf("Process Waiting Time: %ds\n", p->waiting_time);
-	printf("Process Turnaround time: %.2f\n", p->turnaround_time);
-	printf("Removing from queue\n\n");
+		
+	printf("Process Completion Time: %ds\nProcess Waiting Time: %ds\nProcess Turnaround time: %ds\n"
+	"Removing from queue\n\n", p->completion_time, p->waiting_time, p->turnaround_time);
 }
 
 //--------------------------------------------------------------------------------------------------
 // CPU Methods
 //--------------------------------------------------------------------------------------------------
-
 double cpu_maximum_utilization(CPU *unit) {
-	printf("Unit idle time:%f\n", (double) unit->idle_time);
+	printf("Unit idle time:%.2fs\n", (double) unit->idle_time);
 	return ((double) (unit->total_time_running - unit->idle_time) / (double) (unit->total_time_running)) * 100;
 }
 
@@ -99,7 +88,7 @@ double cpu_maximum_utilization(CPU *unit) {
 // This function returns amount of items currently in the ready queue
 // @returns queue size
 int check_current_size(Queue *queue) {
-    return queue->size;
+    return queue->current_size;
 }
 
 // This functions adds incoming processes or already processed process into queue
@@ -108,11 +97,10 @@ void enqueue(Queue *queue, Process *process) {
 	unsigned currentSize = check_current_size(queue);
    
 	if (queue->arr[currentSize] == NULL && currentSize < size) {
-	   //printf("Inserting...\n");
 	   queue->arr[currentSize] = process;
-	   queue->size++;
+	   queue->current_size++;
 	}
-	queue->original_size = size;
+	queue->original_size = queue->current_size;
 }
 
 // This function either: 
@@ -121,7 +109,7 @@ void enqueue(Queue *queue, Process *process) {
 // Problems: Does not make it null
 void dequeue(Queue *queue, CPU *processor, time_t *start_exec) {
 	queue->front = 0;
-	queue->rear = queue->size-1;
+	queue->rear = queue->current_size-1;
 	Process *tempProcess = queue->arr[queue->front]; 
 	time_t start, end;
 	
@@ -130,36 +118,33 @@ void dequeue(Queue *queue, CPU *processor, time_t *start_exec) {
 	// Check if the process in front of the queue is not considered NULL
 	// Move front process to the end
 	if (tempProcess != NULL) {
-		for (size_t i = 0; i < queue->size-1; i++) {
+		for (size_t i = 0; i < queue->current_size-1; i++) {
 			queue->arr[i] = queue->arr[i+1];
-			//printf("--\n");
 		}
 		queue->arr[queue->rear] = tempProcess;
 	}
 	// Since it's null, dequeue it permanently
 	else {
-		for (size_t i = 0; i < queue->size-1; i++) {
+		for (size_t i = 0; i < queue->current_size-1; i++) {
 			queue->arr[i] = queue->arr[i+1]; 
 		}
-		queue->size--;
+		queue->current_size--;
 		printf("Process has been removed from queue\n");
 	}
 	
 	// Get end clock of when context switch ends...
 	end = clock();
 	
-	printf("Start: %d, End %d\n", *start_exec, end);
-	
+	// Then assign the difference divided by the clocks per second to the idle time.
 	float val = (float) (end - *start_exec) / CLOCKS_PER_SEC;
-	// Assign the difference divided by the clocks per second to the idle time.
+	
 	processor->idle_time += val;
-	printf("Start exec is: %d\n", processor->idle_time);
-
+	printf("Idle time of process is: %d\n", processor->idle_time);
 }
 
 // After the program runs for specified time quantum, check the current state of all processes
 void getCurrentState(Queue *q) {
-	for (size_t i = 0; i < q->size; i++) {
+	for (size_t i = 0; i < q->current_size; i++) {
 		printf("Process id#: %d | Arrival Time: %d | Burst Time %d\n", q->arr[i]->pid, 
 		q->arr[i]->arrival_time, q->arr[i]->burst_time);
 	}
@@ -167,8 +152,8 @@ void getCurrentState(Queue *q) {
 
 // Sorts the queue based on the arrival time of the read processes from the csv file
 void sortQueue(Queue *q) {
-	for (size_t i = 0; i < q->size; i++) {
-		for (size_t j = 0; j < q->size-1; j++) {
+	for (size_t i = 0; i < q->current_size; i++) {
+		for (size_t j = 0; j < q->current_size-1; j++) {
 			if (q->arr[j+1]->arrival_time < q->arr[j]->arrival_time) {
 				Process *p = q->arr[j];
 				q->arr[j] = q->arr[j+1];
@@ -189,14 +174,16 @@ Process *process_pids(unsigned *p_id, unsigned *arrival, unsigned *burst) {
     p->pid = *p_id;
     p->arrival_time = *arrival;
     p->burst_time = *burst;
-    //p->turnaround_time = p->completion_time = p->waiting_time = 0;
+    p->turnaround_time = 0;
+    p->completion_time = 0;
+    p->waiting_time = 0;
     return p;
 }
 
 // Read csv file and check each character for an int. 
 // that int is then passed to the process in the queue array.
 // @input - file name
-void read_csv(Queue *ready_queue) {
+void read_csv(Queue *ready_queue, char *fileName) {
 
     char c;
     FILE *file; // Pointer to file to be read
@@ -204,7 +191,7 @@ void read_csv(Queue *ready_queue) {
     unsigned process_id, arrival_time, burst_time;
     unsigned index = 0;
 
-    file = fopen("pid.csv","r"); // Read the csv file
+    file = fopen(fileName,"r"); // Read the csv file
 
 	// Check if file exists and if it does, continue reading ntil end of file
 	if (file) {
@@ -247,31 +234,43 @@ void start_scheduler(CPU *processor, Queue *ready_queue, unsigned *time_quantum)
 		if (newProcess->burst_time > 0) {
 			printf("Processing...\n");
 			
-			
-			//time_t begin_execution = clock();
+			// Run process on CPU
 			newProcess->burst_time = compute_burst_time(newProcess, time_quantum);
+			
+			// Get time at which process finishes running on CPU
 			time_t end_execution = clock() / CLOCKS_PER_SEC;
+
 			printf("\n");
 			
+			// Get time for how long CPU is idle
+			context_switch_start = clock();
 			getCurrentState(ready_queue);
 			printf("\n");
+			printf("Current time in seconds: %.2fs\n", (double) (clock() - processor->total_time_running) / CLOCKS_PER_SEC);
+
+			// Check if the burst time is zero
 			if (newProcess->burst_time == 0) {
-				double execution_time = (double) (end_execution - processor->total_time_running);
-				//printf("Execution time variable: %f\n", execution_time);
+				
+				// Fill in processes' turnaround time, completion time, etc.
+				int execution_time = end_execution - (int) processor->total_time_running;
 				calculate_process_info(newProcess, &execution_time);
 				
+				// If this process had some type of context switch event, add number of context switches
+				// to total amount that occured.
 				if (newProcess->context_switch_count != 0)	
-					processor->total_context_switches += 1;
+					processor->total_context_switches += newProcess->context_switch_count;
 					
+				// Add process' waiting time to total waiting time
 				processor->total_waiting_time += newProcess->waiting_time;
-				//printf("Current total waiting time: %d\n", processor->total_waiting_time);
+				printf("Current total waiting time: %d\n", processor->total_waiting_time);
+				
+				// Make that item null now
 				ready_queue->arr[ready_queue->front] = NULL;
 			} 
 			else {
 				printf("Context switch...\n");
 				newProcess->context_switch_count += 1;
 			}
-			context_switch_start = clock();
 			dequeue(ready_queue, processor, &context_switch_start);
 			printf("Process ran for time quantum of: %d\nOriginal size of the queue was: %d\n"
 			"Current size of the queue is: %d\nAfter dequeue, state of processes looks as follows.\n", 
@@ -280,13 +279,10 @@ void start_scheduler(CPU *processor, Queue *ready_queue, unsigned *time_quantum)
 			
 			// Set the process equal to whatever is in front now
 			newProcess = ready_queue->arr[ready_queue->front];
-			printf("Current time in seconds: %d", (double)(clock() - context_switch_start) / CLOCKS_PER_SEC);
 			printf("\n");
 			delay(1000);
 		}
-		//processor->total_time_running += clock() / CLOCKS_PER_SEC;
 	}
-	
 }
 
 // This function returns burst time after computing difference between process' burst time and the
@@ -300,10 +296,10 @@ int compute_burst_time(Process *p, unsigned *time_quantum) {
 			delay(1000);
 		}
 	}
-	
 	return p->burst_time;
 }
 
+// Return CPU Utilization, average waiting time, throughput, and # of occurances of context switching
 void analysis(CPU *unit, Queue *ready_queue) {
 	// Get end duration of the clock and subtract the end from the beginning to get the total run time
 	time_t end = clock() / CLOCKS_PER_SEC;
@@ -311,8 +307,8 @@ void analysis(CPU *unit, Queue *ready_queue) {
 	double average_waiting_time = unit->total_waiting_time / ready_queue->original_size;	
 	
 	printf("This took %.2lfs\n", (double)unit->total_time_running);
-	printf("CPU Utilization: %.2fs\n", cpu_maximum_utilization(unit));
-	printf("Total Waiting Time: %.2f\n", average_waiting_time);
+	printf("CPU Utilization: %.2f%\n", cpu_maximum_utilization(unit));
+	printf("Average Waiting Time: %.2fs\n", average_waiting_time);
 	printf("Total throughput: %.2f\n", ready_queue->original_size / (double) (unit->total_time_running));	
 	printf("Total amount of context switches: %d\n", unit->total_context_switches);
 }
@@ -334,12 +330,14 @@ void delay(int ms) {
         current = clock();
 }
 
-int main() {
-	// Originally initialize Array to contain null values
+int main(int argc, char *argv[]) {
+	
+	// Initialize items in ready_queue to be null, and the variables in CPU to be 0.
     Queue ready_queue = {NULL};
     CPU processing_unit = {0, 0, 0, 0 ,0};
-    read_csv(&ready_queue);
-	//printf("Size is: %d\n", check_current_size(&ready_queue));
+    
+    // Read the argument, which is a csv file.
+    read_csv(&ready_queue, argv[1]);
     sortQueue(&ready_queue);
     
     printf("Enter the time quantum of your choice: ");
@@ -347,10 +345,10 @@ int main() {
     printf("Your time quantum is: %d\n", time_quantum);
     printf("Size is: %d\nProcesses loaded into ready queue:\n", check_current_size(&ready_queue));
     
-    processing_unit.total_time_running = clock() / CLOCKS_PER_SEC; 
-    getCurrentState(&ready_queue);
-    
+    getCurrentState(&ready_queue);    
     printf("\n");
+    
+	processing_unit.total_time_running = (int) clock() / CLOCKS_PER_SEC; 
 	start_scheduler(&processing_unit, &ready_queue, &time_quantum);
 	printf("Done processing\n\n");
 	
